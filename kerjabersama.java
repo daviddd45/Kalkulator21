38d361ae8b056a389b36dd44934f91ae9e0085e6
daviddd45 <152369749+daviddd45@users.noreply.github.com>
2024-05-15 21:05:04 +0700

    Create kerjabersama.java
    first project

---
+++ kerjabersama.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class CalculatorToko {
+public static void main(String[] args) {
+Scanner scanner= new Scanner(System.in);
+
+System.out.print("Masukan harga barang =");
+double hargaBarang=scanner.nextDouble();
+
+System.out.println("subTotal :" + subTotal);
+System.out.println("pajak :" + pajak);
+System.out.println("tips :" + tips);
+System.out.println("total :" + total);
+}
+}
